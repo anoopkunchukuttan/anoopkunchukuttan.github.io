29001e7edb1c2f5cdec4ea3ac43991cc0583ceb7
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2025-03-02 12:47:21 +0530

    update

---
+++ assets/js/_publications.js
@@ -0,0 +1,24 @@
+function filterList(keyword) {
+    let items = document.querySelectorAll('.publication-item');
+    let years = document.querySelectorAll('.year-header');
+    let visibleYears = new Set();
+    
+    items.forEach(item => {
+        let classes = item.className.split(" ");
+        if (keyword === 'all' || classes.includes(keyword.replace(" ", "_"))) {
+            item.style.display = '';
+            visibleYears.add(classes[classes.length - 1]); // Capture the year class
+        } else {
+            item.style.display = 'none';
+        }
+    });
+    
+    years.forEach(year => {
+        let yearClass = year.classList[1];
+        if (visibleYears.has(yearClass)) {
+            year.style.display = '';
+        } else {
+            year.style.display = 'none';
+        }
+    });
+}
